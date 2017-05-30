0e38608361753da8deb9550b3eb42303374e71fb
Richard-Thompson <richardthompson1990@hotmail.co.uk>
2017-05-30 17:18:24 +0100

    changed the test url

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   DB: {
-    test: 'mongodb://localhost/northcoders-news-api-test',
+    test: 'mongodb://ncnews:ncnews@ds129179.mlab.com:29179/north_coders_news',
     dev: 'mongodb://ncnews:ncnews@ds129179.mlab.com:29179/north_coders_news'
   },
   PORT: {
-    test: 3090,
+    test: 3000,
     dev: process.env.PORT || 3000
   }
 };
